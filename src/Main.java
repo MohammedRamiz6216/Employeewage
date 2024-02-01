d01565a0e9f6afd429026736274dd605d959b00f
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-01 11:04:32 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ public class Main {
         System.out.println("Welcome to Employee Wage Computation");
 
         int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR = 20;
+
+        int empHrs=0;
+        int empWage=0;
 
 
         int random_int = (int)Math.floor(Math.random() *10)%2;
         if(random_int == IS_FULL_TIME)
-            System.out.println("Present");
+            empHrs=8;
         else
-            System.out.println("Absent");
+            empHrs=0;
+        empWage= empHrs*EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage :"+empWage);
 
 
     }
