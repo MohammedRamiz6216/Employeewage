16380e331f457fd4bd1535c1fa11d5a114b2541d
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-01 11:44:40 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,17 @@ public class Main {
     public static final int IS_FULL_TIME=2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int MAX_HOURS_IN_MONTH = 10;
     public static void main(String[] args) {
 
         int empHrs=0;
-        int empWage=0;
-        int totalEmpWage=0;
+        int totalEmpHours=0;
+
+        int totalWorkingDays=0;
 
 
-for(int i=0;i<NUM_OF_WORKING_DAYS;i++) {
+while(totalEmpHours<=MAX_HOURS_IN_MONTH && totalWorkingDays <=NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     int random_int = (int) Math.floor(Math.random() * 10) % 3;
     switch (random_int) {
         case IS_PART_TIME:
@@ -26,11 +29,11 @@ for(int i=0;i<NUM_OF_WORKING_DAYS;i++) {
             empHrs = 0;
 
     }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    totalEmpWage+=empWage;
-    System.out.println("Emp Wage :" + empWage);
+    totalEmpHours += empHrs ;
+    System.out.println("Days :" + totalWorkingDays+"Hours :"+totalEmpHours);
 }
-        System.out.println("Total emp wage :"+totalEmpWage);
+int totalEmpWage = totalEmpHours*EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage :"+totalEmpWage);
 
     }
 }
