0b3374e2e751a4d0bcc0ed534823aa6f02939ec5
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-01 09:33:23 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        int ispresent =0;
+        int isabsent =1;
+
+        int random_int = (int)Math.floor(Math.random() *(isabsent - ispresent + 1) + ispresent);
+        if(random_int == 0){
+            System.out.println("Absent");
+        }
+        else{
+            System.out.println("Present");
+        }
 
     }
 }
