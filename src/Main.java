ac03983aecb1c51b1224f0361bc1802080d055a7
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-01 12:41:39 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import java.util.*;
 public class Main {
    public static final int IS_PART_TIME =1;
     public static final int IS_FULL_TIME=2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS=20;
-    public static final int MAX_HOURS_IN_MONTH = 100;
-    public static int computeEmpWage(){
+
+    public static int computeEmpWage(String company,int empRatePerHour,int noOfWorkingDays,int maxHoursPerMonth){
 
         int empHrs=0;
         int totalEmpHours=0;
@@ -13,7 +11,7 @@ public class Main {
         int totalWorkingDays=0;
 
 
-while(totalEmpHours<=MAX_HOURS_IN_MONTH && totalWorkingDays <NUM_OF_WORKING_DAYS){
+while(totalEmpHours<=maxHoursPerMonth && totalWorkingDays <noOfWorkingDays){
     totalWorkingDays++;
     int random_int = (int) Math.floor(Math.random() * 10) % 3;
     switch (random_int) {
@@ -33,11 +31,18 @@ while(totalEmpHours<=MAX_HOURS_IN_MONTH && totalWorkingDays <NUM_OF_WORKING_DAYS
     System.out.println("Days :" + totalWorkingDays+"Hours :"+totalEmpHours);
 
 }
-int totalEmpWage = totalEmpHours*EMP_RATE_PER_HOUR;
+int totalEmpWage = totalEmpHours*empRatePerHour;
         System.out.println("Total Emp Wage :"+totalEmpWage);
         return totalEmpHours;
     }
     public static void main(String[] args){
-        computeEmpWage();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+         String s = sc.next();
+         int emp    = sc.nextInt();
+         int num = sc.nextInt();
+         int max = sc.nextInt();
+        computeEmpWage(s,emp,num,max);}
     }
 }
