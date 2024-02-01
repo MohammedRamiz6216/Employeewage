ea8c6172698a24f3dce6edf9b4129d059a8cfe08
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-01 10:03:03 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,26 @@ public class Main {
         System.out.println("Welcome to Employee Wage Computation");
         int ispresent =0;
         int isabsent =1;
+        int fdew=1;
+        int pdew=2;
         int wph = 20;
-        int fdh = 8;
+
+
+
         int random_int = (int)Math.floor(Math.random() *(isabsent - ispresent + 1) + ispresent);
+        int random_in = (int)Math.floor(Math.random() *(pdew-fdew+1)+fdew);
         if(random_int == 0){
             System.out.println("Absent");
         }
         else{
             System.out.println("Present");
         }
-        System.out.println("The daily employee wage is :"+ wph*fdh);
+        if(random_in==fdew){
+            System.out.println("The daily employee wage is :"+wph*8);
+        }
+        else{
+            System.out.println("The part time employee wage is :"+ wph*8);
+        }
+        System.out.println("The daily employee wage is :"+ fdew);
     }
 }
