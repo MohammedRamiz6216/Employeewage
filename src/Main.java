cc18594f3411d6506b74a3fe37a63db33a1bec52
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-01 11:20:09 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import java.util.*;
 public class Main {
+   public static final int IS_PART_TIME =1;
+    public static final int IS_FULL_TIME=2;
+    public static final int EMP_RATE_PER_HOUR = 20;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-        int IS_PART_TIME =1;
-        int IS_FULL_TIME=2;
-        int EMP_RATE_PER_HOUR = 20;
 
         int empHrs=0;
         int empWage=0;
@@ -13,12 +12,19 @@ public class Main {
 
 
         int random_int = (int)Math.floor(Math.random() *10)%3;
-        if(random_int == IS_PART_TIME)
-            empHrs=4;
-        else if(random_int==IS_FULL_TIME)
-            empHrs=8;
-        else
-            empHrs=0;
+       switch (random_int){
+           case IS_PART_TIME :
+               empHrs=4;
+               break;
+
+           case IS_FULL_TIME :
+               empHrs=8;
+               break;
+
+           default:
+               empHrs=0;
+
+       }
         empWage= empHrs*EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage :"+empWage);
 
