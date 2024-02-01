035128cf7833aa817b92b9f136f98268f073d16e
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-01 10:54:41 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
-        int Is_Full_Time=1;
+        int IS_FULL_TIME=1;
 
 
         int random_int = (int)Math.floor(Math.random() *10)%2;
-        if(random_int == Is_Full_Time)
+        if(random_int == IS_FULL_TIME)
             System.out.println("Present");
         else
             System.out.println("Absent");
